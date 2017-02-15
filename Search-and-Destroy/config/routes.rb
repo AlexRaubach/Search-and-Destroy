2a5fff2b6c23cfae2681e36633c3fca2160ca3c8
Alex Raubach <Antigrapist@users.noreply.github.com>
2017-02-15 12:26:39 -0600

    Removed duplicated routes

--- Search-and-Destroy/config/routes.rb
+++ Search-and-Destroy/config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  # get 'games/index'
-
-  # get 'games/new'
-
-  # get 'games/show'
-
-  # get 'gamelobby/index'
 
   resources :users
   resources :games
 
   root 'games#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
