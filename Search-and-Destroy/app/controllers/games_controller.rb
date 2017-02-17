4f23276516ee2a2ee1d8bf41742ae1d327862b7d
Larry Cherry <larry.cherry.0406@gmail.com>
2017-02-17 15:26:14 -0600

    fixing merge issue

--- Search-and-Destroy/app/controllers/games_controller.rb
+++ Search-and-Destroy/app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  include GamesHelper
   def index
     @games = Game.all
   end
@@ -16,22 +17,13 @@ class GamesController < ApplicationController
   end
 
   def show
-    p session[:user_id]
-    p params[:id]
     @game = Game.find(params[:id])
-    # @board = @game.boards.first
     if @game.boards.count >= 2
-      if @game.boards.first.user_id == session[:user_id]
-        @my_board = @game.boards.first
-        @opponent_board = @game.boards.last
-      else
-        @my_board = @game.boards.last
-        @opponent_board = @game.boards.first
-      end
+      @my_board = set_my_board(@game, session[:user_id])
+      @opponent_board = set_opponent_board(@game, session[:user_id])
     else
       redirect_to "/games/#{@game.id}/waiting"
     end
-
   end
 
   def delete
@@ -45,13 +37,7 @@ class GamesController < ApplicationController
   def update
     @game = Game.find(params[:id])
     if @game.current_player_id != session[:user_id]
-      if @game.boards.first.user_id == session[:user_id]
-          @my_board = @game.boards.first
-          @opponent_board = @game.boards.last
-        else
-          @my_board = @game.boards.last
-          @opponent_board = @game.boards.first
-        end
+      @opponent_board = set_opponent_board(@game, session[:user_id])
       @opponent_board.fire_on(params[:attack_coordinates])
       @opponent_board.save
       @game.current_player_id = session[:user_id]
