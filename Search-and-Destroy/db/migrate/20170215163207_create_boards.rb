fd8a0dc241906ded5bb318afb1bbb48bd1d4d00d
Joshua Lucas <email": null>
2017-02-17 16:06:00 -0600

    add working firing on ships

--- Search-and-Destroy/db/migrate/20170215163207_create_boards.rb
+++ Search-and-Destroy/db/migrate/20170215163207_create_boards.rb
@@ -4,11 +4,11 @@
       t.integer :user_id
       t.integer :game_id
       t.json :board_state
-      t.string :patrol_location, default: '0,0'
-      t.string :sub_location, default: '0,0'
-      t.string :carrier_location, default: '0,0'
-      t.string :battleship_location,default: '0,0'
-      t.string :destroyer_location, default: '0,0'
+      t.string :patrol_location, default: 'A0 A1'
+      t.string :sub_location, default: 'B0 B1 B2'
+      t.string :carrier_location, default: 'C0 C1 C2 C3 C4'
+      t.string :battleship_location,default: 'D0 D1 D2 D3'
+      t.string :destroyer_location, default: 'E0 E1 E2'
 
       t.timestamps
     end
